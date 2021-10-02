fix bugs in gumbel softmax

fairseq/modules/moe/topkgate.py
@@ -52,9 +52,9 @@ def topkgating(
     num_experts = logits.shape[1]    
     if parameter.gumbel_temperature > 0:
         if parameter.soft_gumbel_training == True:
-            gates = F.gumbel_softmax(logits, tau=parameter.gumbel_temperature, hard=False)
+            gates = F.gumbel_softmax(logits.log(), tau=parameter.gumbel_temperature, hard=False)
         else:
-            gates = F.gumbel_softmax(logits, tau=parameter.gumbel_temperature, hard=False)
+            gates = F.gumbel_softmax(logits.log(), tau=parameter.gumbel_temperature, hard=False)
             indices1_s = torch.argmax(gates, dim=1)
             mask1 = one_hot(indices1_s, num_classes=num_experts, unsqueeze_indices=True)
             gates = gates * mask1