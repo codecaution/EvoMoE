e2f41078caf9bdf1c4c12ca6b962c43e7434cda7
codecaution <xiaonan.nie@pku.edu.cn>
2021-10-16 15:04:36 +0800

    modify switch script

--- scripts/pretrain_gptxl_switch.sh
+++ scripts/pretrain_gptxl_switch.sh
@@ -3,14 +3,16 @@ echo -e "\n\n\n\n"
 echo "=====================================ARGS======================================"
 echo "data_name: ${DATA_NAME}"
 echo "batch size: ${BATCH_SIZE}"
-echo "num_experts: ${NUM_EXPERTS}"
 echo "max updates: ${MAX_UPDATES}"
 echo "update_freq: ${UPDATE_FREQ}"
-echo "warmup_steps: ${WARMUP_STEPS}"
-echo "moe_freq: ${MOE_FREQ}"
 echo "lr: ${LR}"
+echo "min_lr: ${MIN_LR}"
+echo "warmup_steps: ${WARMUP_STEPS}"
+echo "decay_steps: ${DECAY_STEPS}"
 echo "checkpoint_frequency: ${CHECKPOINT_FREQUENCY}"
-
+echo "validate_frequency: ${VALIDATE_FREQUENCY}"
+echo "num_experts: ${NUM_EXPERTS}"
+echo "moe_freq: ${MOE_FREQ}"
 echo -e "\n\n\n\n"
 echo "=====================================PATH========================================"
 
@@ -71,23 +73,24 @@ python train.py ${ddp_options} \
       --arch transformer_lm_gptmedium_moe \
       --share-decoder-input-output-embed \
       --tokens-per-sample ${TOKENS_PER_SAMPLE} --batch-size ${BATCH_SIZE} --update-freq ${UPDATE_FREQ} \
-      --lr ${LR} --lr-scheduler polynomial_decay --warmup-updates ${WARMUP_STEPS}  \
+      --lr ${LR} --min-lr ${MIN_LR} --warmup-updates ${WARMUP_STEPS} --lr-period-updates ${DECAY_STEPS}\
+      --lr-scheduler cosine --warmup-init-lr 1e-07 --lr-shrink 1 \
       --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-08 \
-      --clip-norm 5.0 --weight-decay 0.1 --dropout 0.1 \
+      --clip-norm 1.0 --weight-decay 0.1 --dropout 0.1 \
       --criterion moe_cross_entropy --moe-gate-loss-wt 0.01 --moe-gate-loss-combine-method sum \
       --moe-expert-count $NUM_EXPERTS --moe-freq $MOE_FREQ \
       --moe-gating-use-fp32 --moe-top1-expert \
-      --moe-normalize-expert-grad sqrt_world_size \
+      --moe-normalize-expert-grad $EXPERT_NORMALIZE\
       --moe-eval-capacity-token-fraction -1.0 \
       --write-checkpoints-asynchronously \
       --save-dir ${CHECKPOINT_PATH} \
       --save-interval-updates ${CHECKPOINT_FREQUENCY} \
       --num-workers ${DLWS_NUM_WORKER}\
-      --ddp-backend fully_sharded \
+      --ddp-backend fully_sharded --no-reshard-after-forward\
       --checkpoint-activations \
       --max-update ${MAX_UPDATES} \
       --total-num-update ${MAX_UPDATES} \
-      --validate-interval-updates ${CHECKPOINT_FREQUENCY} \
-      --log-format json --log-interval 100 \
+      --validate-interval-updates ${VALIDATE_FREQUENCY} \
+      --log-format json --log-interval 500 \
       --symlink \
       --seed 1234 2>&1 | tee -a $LOG_PATH
